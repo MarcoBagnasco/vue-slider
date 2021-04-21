498a65e7bc8b2d33915528769cb26b5a0fa5113f
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-21 16:42:23 +0200

    add logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log(Vue);
+// VUE INSTANCE
+const app = new Vue({
+    el: '#root',
+    data:{
+        images: [
+            'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&amp;dl=clouds-country-daylight-371633.jpg&amp;fm=jpg',
+            'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+            'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+            'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg',
+        ],
+        indexImg: 0,
+    },
+    methods:{
+        prevImg(){
+            this.indexImg--;
+            if(this.indexImg < 0){
+                this.indexImg = this.images.length - 1;
+            }
+        },
+        nextImg(){
+            this.indexImg++;
+            if(this.indexImg > this.images.length - 1){
+                this.indexImg = 0;
+            }
+        },
+        dotNav(index){
+            this.indexImg = index;
+        }
+    }
+});
